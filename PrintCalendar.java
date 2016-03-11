0b5d67a7852785fdb4d8c971fa286657606cca5c
U10416040 <u10416040@go.utaipei.edu.tw>
2016-03-11 22:41:15 +0800

    Final code with comments
    Delete the old code.
    Add comments to the new ones.

--- PrintCalendar.java
+++ PrintCalendar.java
@@ -1,3 +1,5 @@
+//U10416040 ªL¦ö¼á
+
 import java.util.*;
 
 public class PrintCalendar {
@@ -5,14 +7,13 @@ public class PrintCalendar {
 	
 	public static void main(String[] args){
 		Scanner input=new Scanner(System.in);
-		Gcalendar = new GregorianCalendar();
-		Date date = new Date();
+		Gcalendar = new GregorianCalendar();//create GregorianCalendar object 
+		Date date = new Date();//create Date object
 		System.out.println(date.toString());
 		
+		Gcalendar.setTime(date);//set the current date to GregorianCalendar object
 		
-		Gcalendar.setTime(date);
-		
-		Gcalendar.set(Calendar.DAY_OF_MONTH, 1);
+		Gcalendar.set(Calendar.DAY_OF_MONTH, 1);//set the first day  of the month
 		
 		System.out.print("Enter full year(e.g., 2012):");
 		int year=input.nextInt();
@@ -82,76 +83,24 @@ public class PrintCalendar {
 			if((i+startDay)%7==0){
 				System.out.println();
 			}
-		}
-		
+		}	
 		System.out.println();
 	}
 	
-	public static int getStartDay(int year, int month){
-		/*
-		final int START_DAY_FOR_JAN_1_1800=3;
-		int totalNumberOfDays=getTotalNumberOfDays(year, month);
-		
-		return(totalNumberOfDays+START_DAY_FOR_JAN_1_1800)%7;
-		*/
-		
-		//
-		int D_week = Gcalendar.get(Calendar.DAY_OF_WEEK);
-		return D_week-1;
-		//
+	public static int getStartDay(int year, int month){		
+		int D_week = Gcalendar.get(Calendar.DAY_OF_WEEK);//get the day of week
+		return D_week-1;//calendar day is one day more than original day
 	}
-	
-	/*
-	public static int getTotalNumberOfDays(int year, int month){
-		int total=0;
-		
-		
-		for(int i=1800;i<year;i++){
-			if(isLeapYear(i)){
-				total=total+366;
-			}
-			else{
-				total=total+365;
-			}
-		}
-		
-		for(int i=1;i<month;i++){
-			total=total+getNumberOfDaysInMonth(year, i);
-		}
-		
-		
-		return total;
-	}
-	*/
 	
 	public static int getNumberOfDaysInMonth(int year, int month){
-		/*
-		if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12){
-			return 31;
-		}
-		
-		if(month==4 || month==6 || month==9 || month==11){
-			return 30;
-		}
-		
-		if(month==2){
-			return isLeapYear(year)?29:28;
-		}
-		*/
 		int total=0;
 		
+		//to calculate total days of the month, original month is one more than calendar month
 		while (Gcalendar.get(Calendar.MONTH) == month-1) {
 			total++;
-			Gcalendar.add(Calendar.DAY_OF_MONTH, 1);
+			Gcalendar.add(Calendar.DAY_OF_MONTH, 1);//add one day
 		}
 
 		return total;
-	}
-	
-	/*
-	public static boolean isLeapYear(int year){
-		return year%400==0 || (year%4==0 && year%100!=0);
-	}
-	*/
-	
+	}	
 }
